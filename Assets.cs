f5fc10430e4e847cf5bc2f6bc4f8f145ee6ab193
dom2004 <d.magiera-2022@hull.ac.uk>
2024-10-24 17:37:53 +0100

    edited App.xaml file with colour values to prepare UI

---
+++ Assets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SnakeApp
+{
+    public static class Assets
+    {
+        public readonly static ImageSource _Empty = LoadAsset("Empty.png");
+        public readonly static ImageSource _Food = LoadAsset("Food.png");
+        public readonly static ImageSource _Body = LoadAsset("Body.png");
+        public readonly static ImageSource _DeadBody = LoadAsset("DeadBody.png");
+        public readonly static ImageSource _DeadHead = LoadAsset("DeadHead.png");
+        public readonly static ImageSource _Head = LoadAsset("Head.png");
+
+        private static ImageSource LoadAsset(string assetPath)
+        {
+            return new BitmapImage(new Uri($"Assets/{assetPath}", UriKind.Relative));
+        }
+    }
+}
